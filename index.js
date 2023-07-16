e53e55a2f3f7187bd36c9cf394b7be27f144bcbd
Henrique Zimermann <henriquezimermanngeo@gmail.com>
2023-07-16 17:56:59 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+function outcome() {
+
+    let num1 = Number(document.getElementById('num-one').value)
+    let num2 = Number(document.getElementById('num-two').value)
+    let total = 0
+
+    if(document.getElementById('box1').checked){
+       total = num1 + num2 
+    }
+    else if(document.getElementById('box2').checked){
+       total = num1 - num2 
+    }
+    else if(document.getElementById('box3').checked){
+       total = num1 * num2 
+    }
+    else {
+       total = num1 / num2 
+    }
+    document.getElementById('resultArea').innerHTML = 'Result:' + String(total)
+}
+
+function henrique() {
+    let num1 = Number(document.getElementById('num-one').value);
+    let num2 = Number(document.getElementById('num-two').value);
+    let total = 0;
+
+    switch (true) {
+        case document.getElementById('box1').checked:
+            total = num1 + num2;
+            break;
+        case document.getElementById('box2').checked:
+            total = num1 - num2;
+            break;
+        case document.getElementById('box3').checked:
+            total = num1 * num2;
+            break;
+        default:
+            total = num1 / num2;
+    }
+
+    document.getElementById('resultArea').innerHTML = 'Result:' + String(total);
+}
